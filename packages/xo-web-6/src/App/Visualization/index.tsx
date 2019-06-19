f15f47b7f9d999a42d2f4d94e992db5df5f49d85
ouadih <ouadi.hanane@gmail.com>
2019-06-19 13:50:08 +0200

    test recharts

--- packages/xo-web-6/src/App/Visualization/index.tsx
+++ packages/xo-web-6/src/App/Visualization/index.tsx
@@ -1,378 +1,153 @@
-
 import React, { Component } from 'react';
 import moment from 'moment';
-import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
-import { Line, Label, ReferenceArea,PieChart, Pie, Sector, Legend,AreaChart, Area } from 'recharts';
+import { LineChart, CartesianGrid, XAxis, YAxis, ZAxis, Tooltip } from 'recharts';
+import {
+  Line, Label, ReferenceArea, PieChart, ReferenceLine,
+  Brush, Pie, Sector, Legend, AreaChart, Area, ScatterChart, Scatter
+} from 'recharts';
 import dataJson from './dump.json';
+import { any } from 'prop-types';
+import { statement } from '@babel/template';
 
 
 export default class Visualization extends Component<any, any> {
+datax:any=[];
+datay:any=[];
+
+   state:any ={
+
+    line1:{
+        x:[],
+        y:[],
+        name:'Line 1'
+    },
+     line2:{
+       x:[],
+       y:[],
+       name:'Line 2'
+    }, 
+    line3:{
+      x:[],
+      y:[],
+      name:'Line 3'
+    },
+    revision:0,
+  } 
+ 
+  componentDidMount(){
+    setInterval( this.increaseGraphic , 2000);
+  } 
+  rand = () => parseInt(Math.random() * 10 + this.state.revision, 10);
+  
+  increaseGraphic = () => {
+    const { line1, line2,line3} = this.state;
+   /*  var t = new Date();
+    var data00 =[];
+      for(var i=10; i>=0; i--){
+      var x = new Date(t.getTime()-i*3600);
+      data00.push([x]);      
+      } */
+        
+    line1.x.push(this.rand());
+    //line1.x.push(line1.x[0]+this.rand());
+    line1.y.push(this.rand());
+    line2.x.push(line1.x[line1.length-1]+this.rand());
+    line2.y.push(this.rand());
+    line3.x.push(line3.x[line3.length-1]+this.rand());
+    line3.y.push(this.rand());
+    this.setState({ line1,line2,line3})
+    
+  }
 
   constructor(props: any) {
     super(props);
   }
 
   render() {
-
-    //Je recupere les 10 premiers données de cpu0 dans le tableau datay
+    console.log(this.state.line1);
+    //Je recupere les 40 premiers données de cpu0 dans le tableau datay
     var datay: any = [];
-    for (var i = 0; i < 10; i++) {
+     for (var i = 0; i < 40; i++) {
       datay[i] = dataJson.stats.cpus[0][i];
-    }
-
-    //Je recupere les 10 premieres date dans datax
-    let som = 3600;
-    let datax: any = [];
-    for (var j = 0; j < 10; j++) {
-      var d4= moment(som);
-      var result = d4.format('LTS');
-      datax[j]=result;
-      som = som + 3600;
-    }
-
-    //Je recupere les 10 premieres données de cpu1
-    var datacpu1: any = [];
-    for (var i = 0; i < 10; i++) {
-    datacpu1[i] = dataJson.stats.cpus[1][i];
-    }
-
-    //Je recupere les 10 premieres données de cpu2
-    var datacpu2: any = [];
-    for (var i = 0; i < 10; i++) {
-      datacpu2[i] = dataJson.stats.cpus[2][i];
-    }
-
-    //Je recupere les 10 premieres données de cpu3
-    var datacpu3: any = [];
-    for (var i = 0; i < 10; i++) {
-      datacpu3[i] = dataJson.stats.cpus[3][i];
-    }
-
-//je recupere les 10 premiere valeurs de load
-    var dataLoad: any = [];
-    for (var i = 0; i < 10; i++) {
-       dataLoad[i] = dataJson.stats.load[i];
-}
-
-//Je recupere les 10 premières valeurs de pifs: pifs:rx:0
-var dataPif0rx: any = [];
-for (var i = 0; i < 10; i++) {
-  dataPif0rx[i] = dataJson.stats.pifs.rx[0][i];
-}
-
-//Je recupere les 10 premières valeurs de pifs: pifs:rx:1
-var dataPif1rx: any = [];
-for (var i = 0; i < 10; i++) {
-  dataPif1rx[i] = dataJson.stats.pifs.rx[1][i];
-}
-
-//Je recupere les 10 premières valeurs de pifs: pifs:rx:2
-var dataPif2rx: any = [];
-for (var i = 0; i < 10; i++) {
-  dataPif2rx[i] = dataJson.stats.pifs.rx[2][i];
-}
-
-//Je recupere les 10 premières valeurs de pifs: pifs:tx:0
-var dataPif0tx: any = [];
-for (var i = 0; i < 10; i++) {
-  dataPif0tx[i] = dataJson.stats.pifs.tx[0][i];
-}
-
-//Je recupere les 10 premières valeurs de pifs: pifs:tx:1
-var dataPif1tx: any = [];
-for (var i = 0; i < 10; i++) {
-  dataPif1tx[i] = dataJson.stats.pifs.tx[1][i];
-}
-
-//Je recupere les 10 premières valeurs de pifs: pifs:tx:2
-var dataPif2tx: any = [];
-for (var i = 0; i < 10; i++) {
-  dataPif2tx[i] = dataJson.stats.pifs.tx[2][i]; 
-  console.log(dataPif2tx);
-}
-
-//Je recupere les 10 premieres valeurs de iowait:a889b334
-var dataIowait_host1: any = [];
-for (var i = 0; i < 10; i++) {
-  dataIowait_host1[i] = dataJson.stats.iowait["a889b334"][i];
+    } 
   
-}
+    //je cree mon tableau datax, qui contient mes dates
+    let datax: any =[];
+    let interval = dataJson.interval;
+    console.log(interval);
+    let res;
+    let datefin = dataJson.endTimestamp;
+    let step= dataJson.interval;
+    console.log(datefin);
+       for(var j=0; j<40;j++){
+      
+       res= datefin+interval;
+       var d4 = moment(res);
+       var result = d4.format('LTS')
+       datax[j] = result;
+       interval = interval+step;
+     }   
+     //datax = [1558962000,1558965600,1558967200];
+      var t = new Date();
+      for (var i = 10; i >= 0; i--) {     
+         var x = new Date(t.getTime() - i * 1000);
+         datax.push([x]);
+      }  
 
-//Je recupere les 10 premieres valeurs de iowait:660c182f
-var dataIowait_host2: any = [];
-for (var i = 0; i < 10; i++) {
-  dataIowait_host2[i] = dataJson.stats.iowait["660c182f"][i];
-}
-
-//Je recupere les 10 premieres valeurs de iowait:7cd1bf8b
-var dataIowait_host3: any = [];
-for (var i = 0; i < 10; i++) {
-  dataIowait_host3[i] = dataJson.stats.iowait["7cd1bf8b"][i];
-}
-
-//Je recupere les 10 premieres valeurs de iowait:1e91e92d
-var dataIowait_host4: any = [];
-for (var i = 0; i < 10; i++) {
-  dataIowait_host4[i] = dataJson.stats.iowait["1e91e92d"][i];
-}
-
-//Je recupere les 10 premieres valeurs de iowait:9a208896
-var dataIowait_host5: any = [];
-for (var i = 0; i < 10; i++) {
-  dataIowait_host5[i] = dataJson.stats.iowait["9a208896"][i];
-}
-//Iops_w_host1
-
- //Je recupere les 10 premieres valeurs de iops:w:a889b334
-  var dataIops_w_host1: any = [];
-for (var i = 0; i < 10; i++) {
-  dataIops_w_host1[i] = dataJson.stats.iops.w["a889b334"][i];
-}   
-
-//Je recupere les 10 premieres valeurs de iops:r:a889b334
-var dataIops_r_host1: any = [];
-for (var i = 0; i < 10; i++) {
-  dataIops_r_host1[i] = dataJson.stats.iops.r["a889b334"][i];
-}   
-
-//Je recupere les 10 premieres valeurs de ioTroughput:w:a889b334
-var dataIoTrought_w_host1: any = [];
-for (var i = 0; i < 10; i++) {
-  dataIoTrought_w_host1[i] = dataJson.stats.ioThroughput.w["a889b334"][i];
-}   
-
-//Je recupere les 10 premieres valeurs de ioTroughput:r:a889b334
-var dataIoTrought_r_host1: any = [];
-for (var i = 0; i < 10; i++) {
-  dataIoTrought_r_host1[i] = dataJson.stats.ioThroughput.r["a889b334"][i];
-} 
-
-//Je recupere les 10 premieres valeurs de Latency:w:a889b334
-var dataLatency_w_host1: any = [];
-for (var i = 0; i < 10; i++) {
-  dataLatency_w_host1[i] = dataJson.stats.latency.w["a889b334"][i];
-} 
-
-
-//Je recupere les 10 premieres valeurs de Latency:r:a889b334
-var dataLatency_r_host1: any = [];
-for (var i = 0; i < 10; i++) {
-  dataLatency_r_host1[i] = dataJson.stats.latency.r["a889b334"][i];
-} 
-
-    //Je rempli mon tableau data
+     this.state.line1.x = datax;
+     this.state.line2.x = datax;
+     this.state.line3.x=datax;
     var data: any = [];
-    for (var i = 0; i < 10; i++) {
-      data.push({ x: datax[i], cpu0: datay[i], cpu1: datacpu1[i], cpu2: datacpu2[i],
-      cpu3: datacpu3[i],dataLoad: dataLoad[i],Pif0_rx:dataPif0rx[i],
-       Pif1_rx:dataPif1rx[i], Pif2_rx:dataPif1rx[i],Pif0_tx:dataPif0tx[i], 
-       Pif1_tx:dataPif1tx[i], Pif2_tx:dataPif2tx[i], Iowait_host1:dataIowait_host1[i],
-       Iowait_host2:dataIowait_host2[i],Iowait_host3:dataIowait_host3[i],
-       Iowait_host4:dataIowait_host4[i],Iowait_host5:dataIowait_host5[i],
-       Iops_w_host1:dataIops_w_host1[i], Iops_r_host1:dataIops_r_host1[i],
-       IoTroughput_w_host1:dataIoTrought_w_host1[i],
-       IoTroughput_r_host1:dataIoTrought_r_host1[i],
-       Latency_w: dataLatency_w_host1[i],
-       Latency_r: dataLatency_r_host1[i]
-      });
-    }
-
-
+    for (var i = 0; i < 100; i++) {
+    data.push({
+      x: this.state.line1.x[i], cpu0: this.state.line1.y[i],cpu1:this.state.line2.y[i]
+    });
+  }
     return (
       <div>
-      <div>
         <div>
-                  Usage CPU
-     </div>
-        <AreaChart
-          width={680}
-          height={300}
-          // data={global} 
-          data={data}
-          margin={{
-            top: 5, right: 30, left: 20, bottom: 5,
-          }}
+          <button onClick={() => {        
+                
+          }
+          }
 
-        >
-          <CartesianGrid strokeDasharray="5 5" />
+          >/10minutes</button>
+          <button>/jour</button>
+          <button>/10secondes</button>
+          <button>/hour</button>
+          <div>
+            Usage CPU (%)
+          </div>
+          <br></br>
+          <AreaChart
+            width={720}
+            height={300}
+        
+            data ={[
+              this.state.line1,
+              this.state.line2,
+              this.state.line3,
+            ]}
+            margin={{
+              top: 5, right: 30, left: 20, bottom: 5,
+            }}
 
-          <XAxis dataKey="x" />
-          <YAxis label="%" />
-          <Tooltip />
-          <Legend />
-          <Area type="monotone" dataKey="cpu0" stroke="#82ca9d" fill="#82ca9d" activeDot={{ r: 10 }} />
-          <Area type="monotone" dataKey="cpu1" stroke="#000000" fill="#000000"/>
-          <Area type="monotone" dataKey="cpu2" stroke="red" fill="red"/>
-          <Area type="monotone" dataKey="cpu3" stroke="pink" fill="pink"/>
-        </AreaChart>
+          >
+            <CartesianGrid strokeDasharray="5 5" />
+            <XAxis dataKey="x"/>
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Brush dataKey="name" height={30} stroke="#8884d8" />
+            <Area type="monotone" dataKey="y" stroke="red" fill="red" activeDot={{ r:10}} />
+           
+         
+          </AreaChart>
+        </div>
+        <br></br>
+        <br></br>
+        
       </div>
-<br></br>
-<br></br>
-<div>
-        <div>
-                 Load average
-     </div>
-        <AreaChart
-          width={680}
-          height={300}
-          // data={global} 
-          data={data}
-          margin={{
-            top: 5, right: 30, left: 20, bottom: 5,
-          }}
-
-        >
-          <CartesianGrid strokeDasharray="5 5" />
-
-          <XAxis dataKey="x" />
-          <YAxis label="?" />
-          <Tooltip />
-          <Legend />
-          <Area type="monotone" dataKey="dataLoad" stroke="#82ca9d" fill="#82ca9d" activeDot={{ r: 8 }} />
-        </AreaChart>
-      </div>
-<br></br>
-<br></br>
-      <div>
-        <div>
-                  Network throughtput
-     </div>
-        <AreaChart
-          width={680}
-          height={300}
-          // data={global} 
-          data={data}
-          margin={{
-            top: 5, right: 30, left: 20, bottom: 5,
-          }}
-
-        >
-          <CartesianGrid strokeDasharray="5 5" />
-
-          <XAxis dataKey="x" />
-          <YAxis label=" " />
-          <Tooltip />
-          <Legend />
-          <Area type="monotone" dataKey="Pif0_rx" stroke="#82ca9d" fill="#82ca9d" activeDot={{ r: 8 }} />
-          <Area type="monotone" dataKey="Pif1_rx" stroke="#000000" fill="#000000" />
-          <Area type="monotone" dataKey="Pif2_rx" stroke="red" fill="red"/>
-          <Area type="monotone" dataKey="Pif0_tx" stroke="red" fill="red"/>
-          <Area type="monotone" dataKey="Pif1_tx" stroke="blue" fill="blue"/>
-          <Area type="monotone" dataKey="Pif2_tx" stroke="blue" fill="blue"/>
-        </AreaChart>
-      </div>
-      <br></br>
-      <br></br>
-      <div>
-        <div>
-                 Iowait
-     </div>
-        <AreaChart
-          width={680}
-          height={300}
-          // data={global} 
-          data={data}
-          margin={{
-            top: 5, right: 30, left: 20, bottom: 5,
-          }}
-
-        >
-          <CartesianGrid strokeDasharray="5 5" />
-
-          <XAxis dataKey="x" />
-          <YAxis label=" " />
-          <Tooltip />
-          <Legend />
-          <Area type="monotone" dataKey="Iowait_host1" stroke="#000000" fill="#000000" activeDot={{ r: 8 }} />
-          <Area type="monotone" dataKey="Iowait_host2" stroke="#000000" fill="#000000"/>
-          <Area type="monotone" dataKey="Iowait_host3" stroke="blue" fill="blue"/>
-          <Area type="monotone" dataKey="Iowait_host4" stroke="green" fill="green"/>
-          <Area type="monotone" dataKey="Iowait_host5" stroke="red" fill="red"/>
-        </AreaChart>
-      </div>
-
-<br></br>
-<br></br>
-      <div>
-        <div>
-                 Iops
-     </div>
-        <AreaChart
-          width={680}
-          height={300}
-          // data={global} 
-          data={data}
-          margin={{
-            top: 5, right: 30, left: 20, bottom: 5,
-          }}
-
-        >
-          <CartesianGrid strokeDasharray="5 5" />
-
-          <XAxis dataKey="x" />
-          <YAxis label=" " />
-          <Tooltip />
-          <Legend />
-          <Area type="monotone" dataKey="Iops_w_host1" stroke="#000000" fill="#000000" activeDot={{ r: 8 }} />
-          <Area type="monotone" dataKey="Iops_r_host1" stroke="red" fill="red" />
-          
-        </AreaChart>
-      </div>
-
-<br></br>
-<br></br>
-      <div>
-        <div>
-                 IoTroughput
-     </div>
-        <AreaChart
-          width={680}
-          height={300}
-          // data={global} 
-          data={data}
-          margin={{
-            top: 5, right: 30, left: 20, bottom: 5,
-          }}
-
-        >
-          <CartesianGrid strokeDasharray="5 5" />
-
-          <XAxis dataKey="x" />
-          <YAxis label=" " />
-          <Tooltip />
-          <Legend />
-          <Area type="monotone" dataKey="IoTroughput_w_host1" stroke="#000000" fill="#000000" activeDot={{ r: 8 }} />
-          <Area type="monotone" dataKey="IoTroughput_r_host1" stroke="blue" fill="blue" />
-          
-        </AreaChart>
-      </div>
-
-      <div>
-        <div>
-                Latency
-     </div>
-        <AreaChart
-          width={680}
-          height={300}
-          // data={global} 
-          data={data}
-          margin={{
-            top: 5, right: 30, left: 20, bottom: 5,
-          }}
-
-        >
-          <CartesianGrid strokeDasharray="5 5" />
-
-          <XAxis dataKey="x" />
-          <YAxis label=" " />
-          <Tooltip />
-          <Legend />        
-          <Area type="monotone" dataKey="Latency_w" stroke="red" fill="red" activeDot={{ r: 8 }} />
-          <Area type="monotone" dataKey="Latency_r" stroke="#000000" fill="#000000" />        
-        </AreaChart>
-      </div>
-
-</div>
 
     );
   }
